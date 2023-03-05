93597e71a6c814b6fe07fde8cd52e986eb4a963a
SuvorovaSvetlana <suvorova.brs@gmail.com>
2023-03-05 18:03:42 +0300

    coment

--- index.js
+++
@@ -1,8 +0,0 @@
-const input = document.getElementById('basic-url')
-const total = document.getElementById('score')
-let inp = 0
-
-input.addEventListener('keyup', () => {
-    inp = +input.value
-    total.value = inp*2.53
-})
--- java.js
+++
@@ -1,8 +0,0 @@
- let a
- a = 8
- console.log(a)
-  
- const g=6
- console.log(g)
- 
- console.log(a+g)
